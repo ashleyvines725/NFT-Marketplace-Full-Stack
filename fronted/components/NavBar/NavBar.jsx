f77d988fb0a5d8564e4daacaaf3e7969661dba74
ashleyvines725 <ashleyvines725@gmail.com>
2023-09-25 21:42:23 -0700

    setup frontend and make components folder and file.

---
+++ fronted/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React , {useState , useEffect} from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import {MdNotifications} from 'react-icons/md'
+import {BsSearch} from 'react-icons/bs';
+import {CgMenuLeft , CgMenuRight} from "react-icons/cg"
+
+//internal import
+import Style from "./NavBar.module.css";
+import {Discover ,HelpCenter,Profile,Notification,SideBar} from "./index";
+import {Button} from "../componentindex";
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
---
+++ fronted/components/componentindex.js
@@ -0,0 +1,3 @@
+import Button from "./Button/Button"
+import Footer from "./Footer/footer"
+import NavBar from "./NavBar/NavBar"
