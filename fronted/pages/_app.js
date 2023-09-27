052f6214e0d45fc6305c5b56767bc6e8b252f973
ashleyvines725 <ashleyvines725@gmail.com>
2023-09-27 12:18:42 -0700

    footer component is complete.

--- fronted/pages/_app.js
+++ fronted/pages/_app.js
@@ -1,9 +1,10 @@
 import '../styles/globals.css'
-import {NavBar }from '../components/componentindex'
+import {NavBar ,Footer}from '../components/componentindex'
 const MyApp=({ Component, pageProps }) =>
 <div>
 <NavBar/>
  <Component {...pageProps} />
+ <Footer/>
  </div>
 
 export default MyApp
