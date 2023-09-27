2c7c2abacabb2259d660cc6d4833a369084c2069
ashleyvines725 <ashleyvines725@gmail.com>
2023-09-27 13:12:08 -0700

    hero Section is complete

--- fronted/pages/index.js
+++ fronted/pages/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+//internal import
+import {HeroSection} from '../components/componentindex'
+import Style from '../styles/index.module.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    
+    <div className={Style.homePage}>
+      <HeroSection/>
+    </div>
+
   )
 }
 
